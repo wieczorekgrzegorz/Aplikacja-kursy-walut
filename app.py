300dee9034fb1b8957de35192cd5135e09641371
wieczorekgrzegorz <124365703+wieczorekgrzegorz@users.noreply.github.com>
2023-06-05 23:44:04 +0200

    added cache check before API connection

--- app.py
+++ app.py
@@ -1,67 +1,89 @@
 # Import libraries necessary for application
-import csv
-from flask import Flask, render_template, request, after_this_request, send_file
+from flask import Flask, render_template, request, after_this_request
 import requests
 import sqlite3
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib.dates import MO
 import matplotlib.ticker as ticker
 import os
 from datetime import datetime, timedelta
 
 # Set a non-reactive backend for matplotlib to avoid 
 # "Tcl_AsyncDelete: async handler deleted by the wrong thread" error
-matplotlib.use('Agg')  
+matplotlib.use('Agg')
 
-# Define database file path
+# Define database file path and create folder if it does not exist.
 data_folder = "data"
 db_file = os.path.join(data_folder, "currency_rates.db")
-
-# create folder for database if it doesn't exist
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
-# Define static folder file path
 static_folder = 'static'
 chart_file = os.path.join(static_folder, "chart.png")
 
-# create static folder if it doesn't exist
-if not os.path.exists(static_folder):
-    os.makedirs(static_folder) 
-
-
-# Create a list of available currencies by fetching the data from NBP API.
-available_currencies = []
-url = "https://api.nbp.pl/api/exchangerates/tables/a"
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    if data and len(data) > 0:
-        rates = data[0].get("rates")
-        if rates:
-            available_currencies = [rate["code"] for rate in rates]
-            available_currencies.sort()
-            
-
-# initiate Flask app
 app = Flask(__name__)
 
-def fetch_currency_rates(currency:str , start_date:str , end_date:str) -> dict:
-    '''
-    Fetches currency exchange rate from NBP API.
-
-    Parameters:
-        currency (str): Currency code (e.g. USD).
-        start_date (str): Start date in format YYYY-MM-DD.
-        end_date (str): End date in format YYYY-MM-DD.
-
-    Returns:
-        dict: JSON response containing exchange rate information or None if unsuccessful.
-    '''
-    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{start_date}/{end_date}"
+def fetch_available_currencies(): 
+    url = "https://api.nbp.pl/api/exchangerates/tables/a"
+    response = requests.get(url)
     
+    if response.status_code == 200:
+        data = response.json()
+        
+        if data and len(data) > 0:
+            rates = data[0].get("rates")
+            
+            if rates:
+                available_currencies = [rate["code"] for rate in rates]
+                available_currencies.sort()
+                
+    return available_currencies 
+
+def is_data_already_in_cache(currency: str, start_date: str, end_date: str, db_file: str) -> bool:    
+    start_date_date_format = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date_date_format = datetime.strptime(end_date, '%Y-%m-%d').date()  
+    days_difference = (end_date_date_format - start_date_date_format).days
+
+    dates_to_check = []
+    
+    for i in range(days_difference):
+        date_to_check = start_date_date_format + timedelta(days = i)
+        
+        # Weekends excluded as there's no exchange on weekends. 
+        if date_to_check.isoweekday() < 6:
+            dates_to_check.append(date_to_check.strftime('%Y-%m-%d'))
+            
+    conn = sqlite3.connect(db_file, detect_types = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    conn.row_factory = lambda cursor, row: row[0]
+    cached_list = []
+     
+    with conn:   
+        c = conn.cursor()
+        query = '''
+                SELECT 
+                    date,
+                    rate 
+                FROM 
+                    rates 
+                WHERE 
+                    currency = ? AND date BETWEEN ? AND ?
+                ORDER BY
+                    date
+                '''
+        c.execute(query, (currency, start_date, end_date))          
+    cached_list = c.fetchall()
+
+    for date in dates_to_check:
+        if date not in cached_list:
+            print('Requested data not in cache, downloading from NBP API.')
+            return False
+    print('Requested data aready in local db.')
+    return True
+                    
+
+def fetch_currency_rates(currency:str , start_date:str , end_date:str) -> tuple[str, dict]:
+    url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{start_date}/{end_date}"
     response = requests.get(url)
     
     error_message = None
@@ -76,101 +98,89 @@ def fetch_currency_rates(currency:str , start_date:str , end_date:str) -> dict:
         error_message = f'Failure, status code: {response.status_code}.'
         return error_message, None
 
-
-def save_currency_rates_to_db(currency:str , currency_rates:dict, db_file:str) -> None:
-    '''
-    Saves fetched exchange rate information to SQLite database.
-
-    Parameters:
-        currency (str): Currency code (e.g. USD).
-        currency_rates (dict): JSON response containing exchange rate information.
-
-    Returns:
-        None
-    '''
-    conn = sqlite3.connect(db_file)
-        
-    with conn:   
+def save_currency_rates_to_db(currency:str, currency_rates:dict, db_file:str) -> None:
+    conn = sqlite3.connect(db_file, detect_types = sqlite3.PARSE_DECLTYPES |
+                             sqlite3.PARSE_COLNAMES)
+    with conn:
         c = conn.cursor()
-        c.execute('''DROP TABLE IF EXISTS rates''')
-        c.execute('''CREATE TABLE IF NOT EXISTS rates(date text,currency text,rate real)''')
+        c.execute('''CREATE TABLE IF NOT EXISTS rates(
+                        date     TIMESTAMP,
+                        currency TEXT,
+                        rate     REAL)
+                        ''')
 
         rows_to_insert = []
         
         for item in currency_rates['rates']:
-            date = item['effectiveDate']
+            date = item['effectiveDate']                      
             rate = item['mid']
             currency = currency
             rows_to_insert.append((date, currency, rate))
         
         c.executemany("INSERT INTO rates VALUES (?, ?, ?)", rows_to_insert)
+        
+        # remove duplicated rows
+        c.execute('''
+                    DELETE 
+                    FROM rates AS r1 
+                    WHERE EXISTS (
+                        SELECT * 
+                        FROM rates AS r2 
+                        WHERE r1.date = r2.date 
+                            AND r1.currency = r2.currency 
+                            AND r1.rowid > r2.rowid
+                            )
+                        ''')
 
-# Function to get currency data from SQLite database.
-def get_currency_data(currency:str, start_date:str, end_date:str, db_file:str) -> list[tuple]:
-    '''
-    Retrieves currency data from SQLite database.
-
-    Parameters:
-        currency (str): Currency code (e.g. USD).
-        start_date (str): Start date in format YYYY-MM-DD.
-        end_date (str): End date in format YYYY-MM-DD.
-
-    Returns:
-        list[tuple]: List of tuples containing date and exchange rate.
-    '''
-    
-    
-    
+def get_data_from_local_db(currency:str, start_date:str, end_date:str, db_file:str) -> list[tuple]:    
     conn = sqlite3.connect(db_file)
      
     with conn:   
         c = conn.cursor()
-        c.execute("SELECT date, rate FROM rates WHERE currency=? AND date BETWEEN ? AND ?", 
-                (currency, start_date, end_date))
+        query = '''
+            SELECT 
+                date, 
+                rate 
+            FROM 
+                rates 
+            WHERE 
+                currency = ? 
+                AND date BETWEEN ? AND ?
+            ORDER BY
+                date
+        '''
+        c.execute(query, (currency, start_date, end_date))
                 
         return c.fetchall()
 
-# Function to generate a chart for the selected currency.
-def generate_chart(selected_currency:str, start_date:str, end_date:str, db_file:str):
-    '''
-    Generates a chart for the selected currency.
-
-    Parameters:
-        currency (str): Currency code (e.g. USD).
-        start_date (str): Start date in format YYYY-MM-DD.
-        end_date (str): End date in format YYYY-MM-DD.
-
-    Returns:
-        None
-    '''
-
-    currency_data = get_currency_data(selected_currency, start_date, end_date, db_file)
-
-    dates = [row[0] for row in currency_data]
-    rates = [row[1] for row in currency_data]
+def generate_chart(currency_table:list[tuple], selected_currency:str) -> None:
+    dates = [row[0] for row in currency_table]
+    rates = [row[1] for row in currency_table]
     
-    # Create figure and axis objects
     fig, ax = plt.subplots()
+    axes_color = '#1f77b4'
+    grid_color = '#e7f6f8'
+    bg_color = '#fcfcfc'
+    fig.set_facecolor(bg_color)
+    ax.patch.set_facecolor(bg_color)
 
-    # Format the dates on the x-axis
+    ax.plot(dates, rates, linewidth = 2, marker = '.', markersize = 7)
+
+    ax.set_ylabel('Exchange Rate [PLN]', fontdict = {'weight': 'bold'})
+    ax.yaxis.label.set_color(axes_color)
+    
     major_locator_x = mdates.AutoDateLocator(interval_multiples = True)        
     ax.xaxis.set_major_locator(major_locator_x)
     minor_locator_x = ticker.MultipleLocator(1)
     ax.xaxis.set_minor_locator(minor_locator_x)
-
-    # Plot the chart
-    ax.plot(dates, rates, linewidth = 2, marker = '.', markersize = 7)
-
-    ax.set_ylabel('Exchange Rate [PLN]', fontdict = {'weight': 'bold'})
+    ax.xaxis.label.set_color(axes_color)
+    
+    ax.tick_params(axis='x', colors=axes_color)
+    ax.tick_params(axis='y', colors=axes_color)
+    plt.xticks(rotation = 45, ha = 'right')
+        
     ax.set_title(f'{selected_currency}/PLN Exchange Rates', fontdict = {'weight': 'bold'})
-    
-    # set fig & style
-    axes_color = '#1f77b4'
-    grid_color = '#e7f6f8'
-    bg_color = '#fcfcfc'
-    
-    fig.set_facecolor(bg_color)
-    ax.patch.set_facecolor(bg_color)
+    ax.title.set_color(axes_color)
     
     ax.grid(visible = True, axis = 'y', color = grid_color, linestyle = ':')
     ax.grid(visible = True, axis = 'x', color = grid_color, linestyle = ':')
@@ -180,81 +190,81 @@ def generate_chart(selected_currency:str, start_date:str, end_date:str, db_file:
     ax.spines['right'].set_color(axes_color)
     ax.spines['left'].set_color(axes_color)
     
-    ax.tick_params(axis='x', colors=axes_color)
-    ax.tick_params(axis='y', colors=axes_color)
-    
-    ax.yaxis.label.set_color(axes_color)
-    ax.xaxis.label.set_color(axes_color)
-    
-    ax.title.set_color(axes_color)
-    
-    plt.xticks(rotation = 45, ha = 'right')
-
-    # Adjust the margins
     plt.tight_layout()
-
-    # Save the chart to a file
     plt.savefig(chart_file)  
-    
     plt.close()
-    
-    return currency_data
 
+def validate_user_input(start_date, end_date):
+    error_message = None
+    start_date_date_format = datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date_date_format = datetime.strptime(end_date, '%Y-%m-%d').date()   
+    
+    try:
+        assert start_date_date_format <= end_date_date_format, "'Start Date' cannot be after 'End Date'."                
+        max_range = timedelta(days = 93)
+        assert end_date_date_format - start_date_date_format <= max_range, "Maximum date range is 93 calendar days."
+
+    except Exception as e:
+        error_message = str(e)
+        return False, error_message
+        
+    return True, error_message
+    
 # Main route '/'
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    '''
-    Renders the index page and handles form submission.
-
-    Returns:
-        rendered template: index.html template.
-    '''
+    available_currencies = fetch_available_currencies()
     
-    error_message = ''
+    error_message = None
     today = datetime.now().date()
     yesterday = today - timedelta(days = 1)
     
     if request.method == 'POST':
         
-        selected_currency = request.form.get('currency')
+        selected_currency = request.form.get('currency')        
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
         
-        start_date = datetime.strptime(request.form.get('start_date'), '%Y-%m-%d').date()
-        end_date = datetime.strptime(request.form.get('end_date'), '%Y-%m-%d').date()
-
-        # Date validation
-        error_message = None
-    
-        try:
-            assert start_date <= end_date, "'Start Date' cannot be after 'End Date'."
-                                
-            max_range = timedelta(days = 93)
-                
-            assert end_date - start_date <= max_range, "Maximum date range is 93 calendar days."
-
-        except Exception as e:
-            error_message = str(e)
-            return render_template('index.html', error_message = error_message, chart_available = False, 
-                                   available_currencies = available_currencies, yesterday = yesterday)
+        input_dates_valid, error_message = validate_user_input(start_date, end_date)
         
-        error_message, currency_rates = fetch_currency_rates(selected_currency, start_date, end_date)
-                
-        if currency_rates:
+        if input_dates_valid == False:
+            return render_template('index.html', 
+                                error_message = input_dates_valid, 
+                                chart_available = False, 
+                                available_currencies = available_currencies, 
+                                yesterday = yesterday
+                                )
+
+        data_already_in_cache = is_data_already_in_cache(selected_currency, start_date, end_date, db_file)
+        
+        if data_already_in_cache == False:
+            error_message, currency_rates = fetch_currency_rates(selected_currency, start_date, end_date)
             save_currency_rates_to_db(selected_currency, currency_rates, db_file)
-            
-            @after_this_request
-            def send_chart(response):
-                generate_chart(selected_currency, start_date, end_date, db_file)
-                return response
-            
-            currency_table = get_currency_data(selected_currency, start_date, end_date, db_file)
-            
-            return render_template('index.html', error_message = error_message, chart_available = True, 
-                                   available_currencies = available_currencies, selected_currency = selected_currency,
-                                   yesterday = yesterday, start_date = start_date, end_date = end_date,
-                                   currency_data = currency_table)
+                    
+        currency_table = get_data_from_local_db(selected_currency, start_date, end_date, db_file)
+
+        @after_this_request
+        def send_chart(response):
+            generate_chart(currency_table, selected_currency)
+            return response
         
-    return render_template('index.html', error_message = error_message, chart_available = False, 
-                           available_currencies = available_currencies, yesterday = yesterday)
+        return render_template('index.html', 
+                                error_message = error_message, 
+                                chart_available = True, 
+                                available_currencies = available_currencies, 
+                                selected_currency = selected_currency,
+                                yesterday = yesterday, 
+                                start_date = start_date, 
+                                end_date = end_date,
+                                currency_data = currency_table
+                                )
+        
+    return render_template('index.html', 
+                           error_message = error_message, 
+                           chart_available = False, 
+                           available_currencies = available_currencies, 
+                           yesterday = yesterday
+                           )
 
 if __name__ == '__main__':
     app.run(debug = False, port = 8000)
